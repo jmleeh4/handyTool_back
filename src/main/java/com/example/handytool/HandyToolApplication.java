89324c3998d4f318c56b1140464177fccad607e0
jihaenglee <jmleeh819@gmail.com>
2022-10-17 23:16:59 +0900

    commit test

---
+++ src/main/java/com/example/handytool/HandyToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.handytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandyToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/handytool/HandyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
