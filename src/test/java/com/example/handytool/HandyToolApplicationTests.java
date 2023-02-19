57ef5957c7e1490ef32678405ab183dd9bc31b4f
jhleeh4 <jmleeh4@naver.com>
2023-02-20 00:54:12 +0900

    20230220

--- src/test/java/com/example/handytool/HandyToolApplicationTests.java
+++ src/test/java/com/example/handytool/HandyToolApplicationTests.java
@@ -6,8 +6,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HandyToolApplicationTests {
 
+
+
+
     @Test
     void contextLoads() {
+        System.out.println("112");
+        System.out.println("테스트중입니다ㅎㅎ");
+
+    }
+
+    @Test
+    public void testMethod(){
+
+
+        System.out.println("test");
     }
 
 }
